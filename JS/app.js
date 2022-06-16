64ff4e329988d2efae64b58fc82698ba482b25aa
AlenaGlamyty <97868110+AlenaGlamyty@users.noreply.github.com>
2022-06-16 16:24:08 +0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+        function change(element){
+            element.value = 'Ой, я поменял текст!';
+        }
+        function returns(element){
+            element.value = 'Ой, а теперь еще раз поменял!';
+        }
